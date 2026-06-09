4784e5f7878743955495fd5b4d62627429386ed0
SPart <spartakminigaliev@gmail.com>
2026-06-09 07:58:31 +0500

    Delete src directory

--- src/tween.js
+++
@@ -1,9 +0,0 @@
-export function tween(element, startvalue, endvalue, frames) {
-    const inbeetwen = endvalue - startvalue;
-    const step = inbeetwen/frames;
-    return (currentframe) => {
-        if (currentframe < frames) {
-            element.position.x = startvalue + step*currentframe;
-        }
-    }
-}

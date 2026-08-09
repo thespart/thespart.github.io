0668e8595cd19a823d7c42429c3f72e0c5a64793
thespart <spartakminigaliev@gmail.com>
2026-08-09 16:16:45 +0300

    Aaaa

--- animation/gifviewer.js
+++ animation/gifviewer.js
@@ -5,9 +5,9 @@ const blankscreen = document.querySelector("#black");
 const loadingtext = document.querySelector("#gif p");
 const leftbutton = document.querySelector("#buttons #left");
 const rightbutton = document.querySelector("#buttons #right");
-const kolvostuff = 16;
+const kolvostuff = 20;
 
-let latest = 0;
+let latest = Number(localStorage.getItem("choose"));;
 
 blankscreen.classList.add("unblack");
 
@@ -28,12 +28,17 @@ const descriptions = {
     14: "my second commision for subcriber. basically zeroud (guy eating mushroom), cu6e and markys (guys at bg). you can remember them in others gifs aswell",
     15: "cat eating cat. life is strange thing",
     16: "my first animation made in after effects cs6, loved that so much i made gif of this",
+    17: "moment from my animation, you can watch it in my Newgrounds!",
+    18: "monster eating monster. life is strange thing",
+    19: "i had this idea since 2024 and just made it. Could be a good ad for some deodorant",
+    20: "one person and too many eyes. Take it however you want i dont care",
 }
 
+showGIF(latest);
 function showGIF(i) {
     latest = i;
-    gifviewer.src = "./content/" + (i+1) + ".gif";
-        divdesc.textContent = descriptions[i+1];
+    gifviewer.src = "./content/" + (latest+1) + ".gif";
+        divdesc.textContent = descriptions[latest+1];
         if (gifviewer.complete) {
             console.log(gifviewer.src, "loaded");
             loadingscreen(true);
@@ -44,7 +49,7 @@ function showGIF(i) {
             });
         }
 }
-
+// spasi
 function loadingscreen(loaded) {
     if (loaded == true) {
         blankscreen.classList.add("unblack");
@@ -88,21 +93,23 @@ for (let i=0; i<kolvostuff; i++) {
 }
 
 rightbutton.addEventListener("click", () => {
-    if (latest > 0 && latest <= kolvostuff) {
+    if (latest >= 0 && latest < kolvostuff-1) {
         latest++
         showGIF(latest);
     } else {
-        latest = 1;
+        latest = 0;
+        showGIF(latest);
     }
     console.log(latest)
 })
 
 leftbutton.addEventListener("click", () => {
-    if (latest > 0 && latest <= kolvostuff) {
+    if (latest > 0 && latest <= kolvostuff-1) {
         latest--
         showGIF(latest);
     } else {
-        latest = 1;
+        latest = kolvostuff-1;
+        showGIF(latest);
     }
     console.log(latest)
 })
---
+++ main.js
@@ -0,0 +1,100 @@
+const sashkadom = document.querySelector("body header #maininfo #logo #megalogo");
+const news = document.querySelector("body main #rightpart #news");
+const newsElement = document.querySelector('body main #rightpart #manipulator li input');
+const rightbutton = document.querySelector('body main #rightpart #manipulator li #right');
+const leftbutton = document.querySelector('body main #rightpart #manipulator li #left');
+
+let clickable = true;
+sashkadom.addEventListener("click", () => {
+    if (clickable) {
+        clickable = false;
+        sashkadom.classList[0] == "animateSpinner" ? sashkadom.classList.remove("animateSpinner") : sashkadom.classList.add("animateSpinner"); effects();
+        setTimeout(() => {sashkadom.classList.remove('animateSpinner'); clickable=true}, 1000);
+    }
+})
+
+let count = 0;
+const effects = () => {
+    if (count > 60) {
+        count = 0;
+        return;
+    }
+    count++;
+    sashkadom.style.filter = `brightness(${200-count*1.67}%)`;
+    requestAnimationFrame(effects);
+}
+
+
+const NewsJSON = [
+    {s: 'Site Upgrade',t: 'After really long time (2 months) i finally made website look better and now it mobile friendly (<i>kind of</i>) and greenish.. hooray! If u want to see old version <a href="https://web.archive.org/web/20260523083019/https://thespart.ru/">check wayback machine</a>, but for some reason it shows incorrectly and i dont really care. Also, <b>+3 new gifs.</b> Planning to update gifwatcher since it pain to use both on pc and phonr',d: '23.07.2026'},
+    {s: 'b',t: 'водитель маршрутки',d: '05.07.2026'},
+    {s:'Tennis gaem',t: '<a href="https://thespart.ru/tennisball">added new game </a>(its still in beta), andddd in 2 days im going to have birthday :DDDDD!!!!! im going to be XX yo',d: '26.06.2026'},
+    {s: 'Mint chat is gone',t: 'after a while mint chat has died. very sad. also check out my new gifs',d: '19.06.2026'},
+    {s: 'GIFS update',t: "added info to every gif now you can see on what drug i've been while making them",d: '09.06.2026'},
+    {s: 'Little update',t: 'updated site a little, maybe i will remake it one day',d: '01.06.2026'},
+    {s: 'GIFS plans',t: 'also im planning to add info to every of my gifs so you could know why i made em :D',d: '27.04.2026'},
+    {s: 'Initialization',t: "Hello everyone visitors! WELCUM TO MA WEBSITE !!! I will post here sometimes but don't expect me to be very active... or maybe i'll be :)))",d: '25.04.2026'},
+]
+
+// createElement p=parent e=element
+function c(e) {
+    return document.createElement(`${e}`);
+}
+
+//appendchild p=parent e=element
+function a(p, e) {
+    return p.appendChild(e);
+}
+
+// самая страшная функция, да может с реактом или jquery было бы в сто раз
+// быстрее, мне лень
+function addNews(data) {
+    const subject = data.s;
+    const text = data.t;
+    const date = data.d;
+
+    const about = a(news,c('div'));
+    const header = a(about, c('h1'));
+    a(about, c('div')).classList.add('separator');
+    about.id = "about";
+    header.textContent = subject;
+    about.innerHTML += `<h2>${text}</h2>`
+    about.innerHTML += `<h2 id="date">${date}</h2>`
+    return about;
+}
+// news per page
+const NPP = 10;
+let prevnewsvalue = 0;
+let currentnews = 1;
+function addnNews(newsNumber) {
+    for (let i=0; i < NPP; i++) {
+        if (i + (newsNumber-1)*NPP >= NewsJSON.length) {break};
+        addNews(NewsJSON[i + (newsNumber-1)*NPP]);
+    }
+}
+function changeNews() {
+    if (currentnews < 1) {currentnews = 1}
+    if (currentnews > Math.ceil(NewsJSON.length/NPP)) {currentnews = Math.ceil(NewsJSON.length/NPP)};
+    const elements = document.querySelectorAll("#rightpart #about");
+    elements.forEach(el => el.remove());
+    if (prevnewsvalue != currentnews) {news.scrollTo()};
+    addnNews(currentnews);
+    prevnewsvalue = currentnews;
+    newsElement.value = currentnews + `/${Math.ceil(NewsJSON.length/NPP)}`;
+}
+newsElement.addEventListener('input', () => {
+    if (currentnews < 1) {currentnews = 1}
+    if (currentnews > Math.ceil(NewsJSON.length/NPP)) {currentnews = Math.ceil(NewsJSON.length/NPP)};
+})
+newsElement.addEventListener('change', () => {
+    changeNews();
+})
+rightbutton.addEventListener('click', () => {
+    currentnews++;
+    changeNews();
+})
+leftbutton.addEventListener('click', () => {
+    currentnews--;
+    changeNews();
+})
+changeNews();
